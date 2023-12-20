6ca8868a581980bc4d0e243fcc7fff3c51d70b66
sqldatabunny <hitech.bunny.00@gmail.com>
2023-12-20 16:08:57 -0700

    double commit

--- Planned.cs
+++ Planned.cs
@@ -3,6 +3,6 @@
 	public class Planned : Attribute {
 		public string Description;
 		public Planned(string description) => Description = description;
-		public Planned() => Description = "";
+		public Planned() => Description = String.Empty;
 		}
 	}
