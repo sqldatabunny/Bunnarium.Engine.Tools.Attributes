d455bebc7210862673c115e40b696733546aa065
sqldatabunny <hitech.bunny.00@gmail.com>
2023-12-20 18:52:57 -0700

    Various BunnyAttribute removals

--- ArchetypeNeedsReview.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Bunnarium.Engine.Tools.Attributes {
-    [AttributeUsage(AttributeTargets.Class)]
-    public class ArchetypeNeedsReview : Attribute {
-        public string Description;
-        public ArchetypeNeedsReview(string description) => Description = description;
-        public ArchetypeNeedsReview() => Description = "";
-        }
-
-    }
