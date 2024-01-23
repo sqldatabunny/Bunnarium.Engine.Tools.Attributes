7714e6f5da94b7b2cd582fd3637a7e2fda0ed524
sqldatabunny <hitech.bunny.00@gmail.com>
2024-01-22 23:20:25 -0700

    Made vertices generic.

---
+++ VertexSystemChangeMarker.cs
@@ -0,0 +1,10 @@
+﻿namespace Bunnarium.Engine.Tools.Attributes {
+    [AttributeUsage(AttributeTargets.All)]
+    [Remove]
+    public class VertexSystemChangeMarker : Attribute {
+        public string Description;
+        public VertexSystemChangeMarker(string description) => Description = description;
+        public VertexSystemChangeMarker() => Description = string.Empty;
+        }
+
+    }
