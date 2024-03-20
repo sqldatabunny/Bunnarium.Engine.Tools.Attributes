9f53e795ab9ad674f6d10a212618520a1b767cbe
sqldatabunny <hitech.bunny.00@gmail.com>
2024-03-19 18:45:53 -0600

    Added the SemiAutoPropertyCandidate attribute.

---
+++ SemiAutoPropertyCandidate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunnarium.Engine.Tools.Attributes {
+    [AttributeUsage(AttributeTargets.All)]
+    public class SemiAutoPropertyCandidate : Attribute {
+        public string Description;
+        public SemiAutoPropertyCandidate(string description) => Description = description;
+        public SemiAutoPropertyCandidate() => Description = "";
+        }
+    }
