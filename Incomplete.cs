ba013bfadcc92d0c21ae51c0d67eaac7d88e5c6a
sqldatabunny <hitech.bunny.00@gmail.com>
2024-07-28 18:50:37 -0600

    Misc. cleanup

---
+++ Incomplete.cs
@@ -0,0 +1,9 @@
+﻿namespace Bunnarium.Engine.Tools.Attributes {
+    [AttributeUsage(AttributeTargets.All)]
+    public class Incomplete : Attribute {
+        public string Description;
+        public Incomplete(string description) => Description = description;
+        public Incomplete() => Description = "";
+        }
+
+    }
