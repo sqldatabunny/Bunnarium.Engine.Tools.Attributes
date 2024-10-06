c0ced3aca310dddff1cc8efb5c6fc98aee4a55d3
sqldatabunny <hitech.bunny.00@gmail.com>
2024-10-05 21:14:47 -0600

    Worked on IRotation/IDirection interfaces; began acknowledging issues with non-normalized Angle<T> types (potential mismanagement) via a new BunnyAttribute; made generic type parameter requirements demand unmanaged inputs for geometric types that always should have been.

---
+++ NonNormalizedAnglesFlag.cs
@@ -0,0 +1,8 @@
+﻿namespace Bunnarium.Engine.Tools.Attributes {
+    [AttributeUsage(AttributeTargets.All)]
+    public class NonNormalizedAnglesFlag : Attribute {
+        public string Description;
+        public NonNormalizedAnglesFlag(string description) => Description = description;
+        public NonNormalizedAnglesFlag() => Description = "";
+        }
+    }
