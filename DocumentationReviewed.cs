c0b2233bb9815d93f659f31a51da3fdf16bf10f8
sqldatabunny <hitech.bunny.00@gmail.com>
2024-10-13 01:00:28 -0600

    Engine.Maths work

---
+++ DocumentationReviewed.cs
@@ -0,0 +1,8 @@
+﻿namespace Bunnarium.Engine.Tools.Attributes {
+    [AttributeUsage(AttributeTargets.Struct | AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = true, Inherited = false)]
+    public class DocumentationReviewed : Attribute {
+        public string Description;
+        public DocumentationReviewed(string description) => Description = description;
+        public DocumentationReviewed() => Description = "";
+        }
+    }
