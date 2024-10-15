fbff4064900beebf9e22f5515450d7cc2e0b834e
sqldatabunny <hitech.bunny.00@gmail.com>
2024-10-15 17:26:14 -0600

    Documentation infill

--- IPrintable.cs
+++ IPrintable.cs
@@ -1,7 +1,7 @@
 ﻿namespace Bunnarium.Engine.Tools.Attributes {
     public interface IPrintable {
 
-        ///  <returns> A string summarizing this shape's properties.
+        ///  <returns> A formatted string summarizing this item's state.
         /// </returns>
         public string ToString(byte digits, int integerLength, int padToLength);
 
